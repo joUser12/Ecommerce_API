d5706f773bbb571665b5f115658e4ffc6a3a915d
Joysjo1 <joysjobin70@gmail.com>
2023-01-29 19:35:07 +0530

    some error handle added

--- ecommerce/server.js
+++ ecommerce/server.js
@@ -10,3 +10,20 @@ dotenv.config({path:"config/config.env"});
 app.listen(process.env.PORT,()=>{
     console.log(`server listening to the port ${process.env.PORT} in ${process.env.NODE_ENV}`);
 })
+
+
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log(`shutting down the due to unhandled rejection`);
+    server.close(()=>{
+        process.exit(1)
+    })
+})
+
+process.on('uncaughtException',(err)=>{
+    console.log(`Error : ${err.message}`);
+    console.log(`shutting down the due to uncaughtException`);
+    server.close(()=>{
+        process.exit(1)
+    })
+})
