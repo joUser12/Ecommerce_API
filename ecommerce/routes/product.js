f7ad24512c0d92de878671e7fe27abe1a422aac4
Joysjo1 <joysjobin70@gmail.com>
2023-03-02 08:41:30 +0530

    authorisation added

--- ecommerce/routes/product.js
+++ ecommerce/routes/product.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const { getProducts, newProduct,getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 const router = express.Router();
 const {isAuthenticationUser,authorizeRoles } = require ('../middlewares/authenticate')
-router.route('/products').get(getProducts)
+router.route('/products').get(isAuthenticationUser,getProducts)
 router.route('/product/new').post(isAuthenticationUser,authorizeRoles('admin'),newProduct)
 router.route('/product/:id').get(isAuthenticationUser,authorizeRoles('admin'),getSingleProduct)
 .put(isAuthenticationUser,authorizeRoles('admin'),updateProduct)
